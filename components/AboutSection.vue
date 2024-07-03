fb6f809fe91db5b0fd60102784b03d2253171065
adriansyahysf <bimogamerz77@gmail.com>
2024-07-03 23:07:45 +0700

    changing about animation

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -13,23 +13,23 @@
               <div class="About-Skills">
                 <h2>My Skills</h2>
                 <small>HTML</small>
-                <div class="progress mb-3" style="height: 5px">
+                <div class="progress mb-3" style="height: 5px" data-aos="zoom-in" data-aos-duration="3000">
                   <div class="progress-bar bg-primary" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+                </div data-aos="fade-down">
                 <small>CSS</small>
-                <div class="progress mb-3" style="height: 5px">
+                <div class="progress mb-3" style="height: 5px" data-aos="zoom-in" data-aos-duration="3000">
                   <div class="progress-bar bg-primary" role="progressbar" style="width: 70%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+                </div data-aos="fade-down">
                 <small>C++</small>
-                <div class="progress mb-3" style="height: 5px">
+                <div class="progress mb-3" style="height: 5px" data-aos="zoom-in" data-aos-duration="3000">
                   <div class="progress-bar bg-primary" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+                </div data-aos="fade-down">
                 <small>Design</small>
-                <div class="progress mb-3" style="height: 5px">
+                <div class="progress mb-3" style="height: 5px" data-aos="zoom-in" data-aos-duration="3000">
                   <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+                </div data-aos="fade-down">
                 <small>MySQL</small>
-                <div class="progress mb-3" style="height: 5px">
+                <div class="progress mb-3" style="height: 5px" data-aos="zoom-in" data-aos-duration="3000">
                   <div class="progress-bar bg-primary" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
               </div>
@@ -39,27 +39,27 @@
       </div>
     </div>
 
-    <div class="game" data-aos="flip-left">
-      <div class="title-2">
+    <div class="game">
+      <div class="title-2" data-aos="zoom-in">
         <h2>The Games I Play</h2>
       </div>
       <div class="cards">
-        <div class="card">
+        <div class="card" data-aos="flip-up" data-aos-duration="2000">
           <a href="https://playvalorant.com/en-us/download/" target="_blank">
             <img src="assets/img/valorant.jpg" width="100px" height="100" alt="Valorant">
           </a>
         </div>
-        <div class="card">
+        <div class="card" data-aos="flip-up" data-aos-duration="3000">
           <a href="https://wutheringwaves.kurogames.com/en/" target="_blank">
             <img src="assets/img/Wuwa.png" width="100px" height="100" alt="Wuwa">
           </a>
         </div>
-        <div class="card">
+        <div class="card" data-aos="flip-up" data-aos-duration="2000">
           <a href="https://hsr.hoyoverse.com/ua2?lp=sr03&utm_source=SEA_google_SEA_search_UA2_Brand&hoyotrace_channel=ga_channel&gad_source=1&gclid=Cj0KCQjw7ZO0BhDYARIsAFttkCg-yGkTS_fJvCkikpavu6Qo8gv-NnbLf7WybPHZD7GUm83xe2WHXX8aAnPJEALw_wcB" target="_blank">
             <img src="assets/img/HSR.png" width="100px" height="100" alt="Wuwa">
           </a>
         </div>
-        <div class="card">
+        <div class="card" data-aos="flip-up" data-aos-duration="3000">
           <a href="https://osu.ppy.sh/" target="_blank">
             <img src="assets/img/osu.png" width="100px" height="100" alt="Wuwa">
           </a>
